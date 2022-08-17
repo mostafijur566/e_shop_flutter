d52d743ea09d15803c25a2d0476869e10925d128
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-08-18 00:24:25 +0600

    image picker added

--- lib/pages/auth/sign_up_user_details_page.dart
+++ lib/pages/auth/sign_up_user_details_page.dart
@@ -1,7 +1,6 @@
-
-import 'dart:html';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../utils/app_colors.dart';
 import '../../widgets/app_text_field.dart';
@@ -23,112 +22,144 @@ class _SignUpUserDetailsPageState extends State<SignUpUserDetailsPage> {
 
   File? image;
 
+  final _picker = ImagePicker();
+
+  Future getImage() async{
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+
+    if(pickedFile != null){
+      image = File(pickedFile.path);
+      setState(() {
+
+      });
+    }
+    else{
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.25,
-                child: Center(
-                    child: Image.asset('assets/images/logo.png',
-                      width: 170,
-                    )
-                ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
               ),
-            ),
-
-            Container(
-              width: 130,
-              height: 130,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(65),
-                color: Colors.grey[200],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.camera_alt_outlined,
-                    size: 60,
-                    color: AppColors.mainColor,
-                  ),
-                  Text('Upload image', style: TextStyle(color: AppColors.mainColor),)
-                ],
-              )
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppTextField(
-              emailController: nameController,
-              hintText: 'Name',
-              icon: Icons.person,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-
-            AppTextField(
-              emailController: billingAddressController,
-              hintText: 'Billing Address',
-              icon: Icons.location_on,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppTextField(
-              emailController: shippingAddressController,
-              hintText: 'Shipping Address',
-              icon: Icons.location_on,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppTextField(
-              emailController: phoneController,
-              hintText: 'Phone Number',
-              icon: Icons.phone,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            GestureDetector(
-              onTap: () {
-                try {
-                  setState(() {
-
-                  });
-
-                } catch (e) {
-
-                }
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.height / 13,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: AppColors.mainColor),
-                child: Center(
-                  child: BigText(
-                    text: 'Sign Up',
-                    color: Colors.white,
-                    size: 26,
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  child: Center(
+                      child: Image.asset('assets/images/logo.png',
+                        width: 170,
+                      )
                   ),
                 ),
               ),
-            ),
-          ],
+
+              GestureDetector(
+                onTap: (){
+                  getImage();
+                },
+                child: Container(
+                  width: 130,
+                  height: 130,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(65),
+                    color: Colors.grey[200],
+                  ),
+                  child: image == null ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.camera_alt_outlined,
+                        size: 60,
+                        color: AppColors.mainColor,
+                      ),
+                      Text('Upload image', style: TextStyle(color: AppColors.mainColor),)
+                    ],
+                  ) : Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(65),
+                      image: DecorationImage(
+                        image:  FileImage(
+                          File(image!.path).absolute,
+                        ),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                  )
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              AppTextField(
+                emailController: nameController,
+                hintText: 'Name',
+                icon: Icons.person,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              AppTextField(
+                emailController: billingAddressController,
+                hintText: 'Billing Address',
+                icon: Icons.location_on,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              AppTextField(
+                emailController: shippingAddressController,
+                hintText: 'Shipping Address',
+                icon: Icons.location_on,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              AppTextField(
+                emailController: phoneController,
+                hintText: 'Phone Number',
+                icon: Icons.phone,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: () {
+                  try {
+                    setState(() {
+
+                    });
+
+                  } catch (e) {
+
+                  }
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.height / 13,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: AppColors.mainColor),
+                  child: Center(
+                    child: BigText(
+                      text: 'Sign Up',
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
